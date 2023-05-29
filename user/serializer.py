227b9772f198cd24b5a9b0801ff6a629acf86aed
bekirbali <bfbali43@gmail.com>
2023-05-29 22:49:12 +0300

    urls serializer

--- user/serializer.py
+++ user/serializer.py
@@ -3,5 +3,14 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.Serializer):
     class Meta:
-        model=User
+        model = User
         exclude = []
+
+    def validate(self, attrs):
+        if attrs.get('password', False):
+            from django.contrib.auth.password_validation import validate_password
+            from django.contrib.auth.hashers import make_password
+            password = attrs['password']
+            validate_password(password)
+            attrs.update({'password': make_password(password)})
+        return super().validate(attrs)
